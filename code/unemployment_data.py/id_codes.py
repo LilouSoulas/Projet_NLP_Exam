2b93847c67727ba01e9de2318ba0248ff299acaa
lchaudron <127527452+lchaudron@users.noreply.github.com>
2026-04-30 16:22:07 +0200

    Update id_codes.py

--- code/unemployment_data.py/id_codes.py
+++ code/unemployment_data.py/id_codes.py
@@ -5,8 +5,7 @@ df = pd.read_csv('data/taux_chom/id_codes_ts.csv', sep=',')
 df['ID'] = '00' + df['ID'].astype(str)
 
 #print(df.head())
-# On cible les 4 dernières lignes ( -4: ) sur la colonne 'ID'
-# .str[1:] permet de garder la chaîne à partir du deuxième caractère (index 1)
+
 df.iloc[-4:, df.columns.get_loc('ID')] = df.iloc[-4:]['ID'].str[1:]
 
 print(df.tail(6))
